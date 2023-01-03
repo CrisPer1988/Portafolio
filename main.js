ecad36fa6c11ed6dbcd9ef05c2b963ea738c34b8
CrisPer1988 <norberto.cp@hotmail.com>
2023-01-03 11:05:56 -0300

    finish_hamburguer

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 let iconMenu = document.querySelector('#icon__menu');
 let menu = document.querySelector('#content__menu');
 
-iconMenu.addEventListener('click', function(){
+function closeMenu() {
+        menu.classList.toggle('active'); 
+}
 
-        menu.classList.toggle('active');
-       
-})
+iconMenu.addEventListener('click', closeMenu);
+        
+        
+
+let contentMenu = document.querySelectorAll('#content__menu ul li');
+for (const li of contentMenu) {
+        li.addEventListener("click", closeMenu);
+}
